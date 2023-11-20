3355e06fed079bf3780f3809347e88ee700f74cd
truongthuanhung <hung.truongthuan@hcmut.edu.vn>
2023-11-20 10:37:59 +0700

    complete home page

--- script/Home.js
+++ script/Home.js
@@ -1,17 +1,26 @@
-document.querySelector(".header__navbar-icon").addEventListener('click', function() {
-    document.querySelector(".header__navbar-container").classList.toggle("my-hidden");
+document.querySelector(".header__navbar-icon").addEventListener('click', function () {
+    document.querySelector(".header__navbar-container").classList.toggle("custom-hidden");
 })
 
 const navBtns = document.getElementsByClassName("slider__btn");
+const slides = document.getElementsByClassName("slider__container");
 var currentSlide = 0;
 const chooseSlider = (num) => {
     for (let i = 0; i < navBtns.length; ++i) {
         navBtns[i].classList.remove("active");
+        if (!slides[i].classList.contains("my-hidden")) {
+            slides[i].classList.add("my-hidden")
+        }
     }
     if (!navBtns[num].classList.contains("active")) {
         navBtns[num].classList.add("active");
+        slides[num].classList.remove("my-hidden");
         currentSlide = num;
     }
+    clearInterval(autoChange);
+    autoChange = setInterval(() => {
+        next.click();
+    }, 3000)
 }
 
 for (let i = 0; i < navBtns.length; ++i) {
@@ -25,7 +34,7 @@ const next = document.getElementsByClassName("slider__next")[0];
 prev.addEventListener("click", () => {
     if (currentSlide === 0) {
         currentSlide = navBtns.length - 1;
-    } 
+    }
     else {
         currentSlide--;
     }
@@ -35,9 +44,13 @@ prev.addEventListener("click", () => {
 next.addEventListener("click", () => {
     if (currentSlide === navBtns.length - 1) {
         currentSlide = 0;
-    } 
+    }
     else {
         currentSlide++;
     }
     chooseSlider(currentSlide);
 })
+
+var autoChange = setInterval(() => {
+    next.click();
+}, 3000)
